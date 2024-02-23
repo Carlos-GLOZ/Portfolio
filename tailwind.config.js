2731c6066311818f5393facf2fdd1a3019a7988e
Carlos-GLOZ <cgiraldolozano@gmail.com>
2024-02-23 13:17:33 +0100

    banner y menú

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.css",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
