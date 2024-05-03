12b89724074022ab942f2613782ae468cae80a5c
Carlos-GLOZ <cgiraldolozano@gmail.com>
2024-05-03 20:02:50 +0200

    Update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,17 @@ export default {
     fontFamily: {
       'Montserrat': ['Montserrat', 'sans-serif']
     },
+    // backgroundColor: {
+    //   // 'Default': '#1d1d20',
+    //   'Default': '#09080d',
+    //   'Card': '#3d3e42',
+    //   'CardHighlight': '#f0bf6c',
+    // },
     backgroundColor: {
-      'Default': '#1d1d20',
-      'Card': '#3d3e42',
+      'Default': '#222831',
+      'Card': '#393E46',
       'CardHighlight': '#f0bf6c',
-    }
+    },
   },
   plugins: [],
 }
